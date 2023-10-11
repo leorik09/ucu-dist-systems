b3ef9976e755898bddebb2a3c3ed72657aae8523
lev.sikyrynskyi <lsikyrynskyi@nb01cb108.lan>
2023-10-11 21:20:36 +0300

    Inital main setup

---
+++ lev_secondary_server/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+import uvicorn
+
+
+app = FastAPI()
+
+list = []
+@app.get("/all")
+def read_all():
+    return list
+
+@app.post("/append")
+def add_number(num: int):
+    list.append(num)
+
+    import time
+    time.sleep(3)
+
+    return {"message": "Added"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=4000)
