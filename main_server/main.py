34602ed73165c85dd02f19329414c24cbb59d8ca
leorik09 <leorik09@gmail.com>
2023-11-05 18:26:04 +0200

    Add write concern handling

--- main_server/main.py
+++ main_server/main.py
@@ -1,10 +1,13 @@
 import httpx
 import asyncio
+import threading
 from fastapi import FastAPI, HTTPException
 from utils import get_containers_address_in_network, LOGGER
+from datetime import datetime
 
 app = FastAPI()
 data = []
+index_key = 0
 
 
 @app.get("/all")
@@ -12,36 +15,57 @@ def read_all():
     return {"main data": data}
 
 
-async def replicate_on_secondary(client: httpx.AsyncClient, url: str, num: int):
+async def replicate_on_secondary(client: httpx.AsyncClient, url: str, item: dict):
     LOGGER.info(f"Calling {url}.")
     r = await client.post(
         url=url,
-        params={"num": num}
+        timeout=100,
+        params=item
     )
     if r.status_code == 200:
         LOGGER.info(f"Replication to {url} is successful.")
     return r
 
 
-async def replicate_all(hard_urls: list, num: int):
+async def replicate_all(hard_urls: list, item: dict, write_concern: int):
+
     async with httpx.AsyncClient() as client:
-        tasks = [replicate_on_secondary(client, url, num) for url in hard_urls]
-        result = await asyncio.gather(*tasks)
-        LOGGER.info(f'result: {result}')
-    return sum(1 if r.status_code == 200 else 0 for r in result)
+        tasks = [replicate_on_secondary(client, url, item) for url in hard_urls]
+
+        is_completed = 0
+        for task in asyncio.as_completed(tasks):
+            result = await task
+            LOGGER.info(f'Tasks result: {(result)}')
+            is_completed += 1
+            if is_completed == write_concern - 1:
+                LOGGER.info("Successfully waited for wc")
+                return write_concern
+
+
+def run_async_in_thread(hard_urls, num, write_concern):
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(replicate_all(hard_urls, num, write_concern))
+    loop.close()
 
 
 @app.post("/append", status_code=201)
-async def add_number(num: int):
+async def add_number(num: int, write_concern: int):
+    item = {"num": num, "timestamp": str(datetime.now())}
 
     containers = get_containers_address_in_network('data_network')
     hard_urls = [f"http://{container_id}:8000/append" for container_id in containers]
-
-    replication_count = await replicate_all(hard_urls, num)
-
     data.append(num)
 
-    if replication_count != len(containers):
+    replication_count = 1
+
+    if write_concern == 1:
+        thread = threading.Thread(target=run_async_in_thread, args=[hard_urls, item, write_concern])
+        thread.start()
+    else:
+        replication_count = await replicate_all(hard_urls, item, write_concern)
+
+    if replication_count != write_concern:
         raise HTTPException(status_code=500, detail="Unable to replicate")
 
     LOGGER.info("successfully replicated to all nodes")
--- secondary_server/main.py
+++ secondary_server/main.py
@@ -1,6 +1,8 @@
-import time
 import random
 from fastapi import FastAPI
+import logging
+import asyncio
+LOGGER = logging.getLogger(__name__)
 
 app = FastAPI()
 data = []
@@ -8,12 +10,16 @@ data = []
 
 @app.get("/all")
 def read_all():
-    return {"secondary list": data}
+    data_repr = [item["num"] for item in data]
+    return {"secondary list": data_repr}
 
 
 @app.post("/append")
-async def add_number(num: int):
-    data.append(num)
-    time.sleep(random.randint(1,3))
+async def add_number(num: int, timestamp: str):
+    item = {"num": num, "timestamp": timestamp}
+    await asyncio.sleep(random.randint(5,20))
+    data.append(item)
+
+    data.sort(key=lambda x: x["timestamp"])
 
     return {"message": "Added"}
